ed6f60278cc9f30365bd6fb2b0c709064ac9587a
Gordon Rawe <gordon.tongji@hotmail.com>
2015-07-25 20:48:30 +0800

    some change on app.js

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 var express = require('express');
 var app = express();
 
+var data = {beijing:[{name:"changcheng"},{name:"gugong"},{name:"sanlitun"},{name:"yiheyuan"}],
+	shanghai:[{name:"yuyuan"},{name:"waitan"},{name:"dongwuyuan"},{name:"jichang"}],
+	shenzhen:[{name:"huaqiangbei"},{name:"dameisha"},{name:"xiaomeisha"}]};
+
+// var data = {
+// 	cities:[{name:"beijing",places:[{name:"changcheng"},{name:"gugong"},{name:"sanlitun"},{name:"yiheyuan"}]},
+// 	{name:"shanghai",places:[{name:"yuyuan"},{name:"waitan"},{name:"dongwuyuan"},{name:"jichang"}]},
+// 	{name:"shenzhen",places:[{name:"huaqiangbei"},{name:"dameisha"},{name:"xiaomeisha"}]}]
+// }
+
+
+app.get('/cities',function(req,res){
+	res.send(Object.keys(data));
+});
+
+app.get('/cities/:city_name',function(req,res){
+	var city = req.params.city_name;
+	res.send(data[city]);
+});
+
 app.get('/',function(req,res){
 	res.send({name:'gordon',friends:[{name:'nicole'},{name:'lily'}],email:'gordon.tongji@hotmail.com'});
 });
