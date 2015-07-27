0efcf67fab48533cb40c767bcc6c0f9c0f4a38cd
mcluo <mcluo@Ctrip.com>
2015-07-27 10:49:57 +0800

    重新发布地貌服务

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
 var express = require('express');
 var app = express();
+var landscape = require('./landscape');
 
-var data = {beijing:[{name:"changcheng"},{name:"gugong"},{name:"sanlitun"},{name:"yiheyuan"}],
-	shanghai:[{name:"yuyuan"},{name:"waitan"},{name:"dongwuyuan"},{name:"jichang"}],
-	shenzhen:[{name:"huaqiangbei"},{name:"dameisha"},{name:"xiaomeisha"}]};
+// var data = {beijing:[{name:"changcheng"},{name:"gugong"},{name:"sanlitun"},{name:"yiheyuan"}],
+// 	shanghai:[{name:"yuyuan"},{name:"waitan"},{name:"dongwuyuan"},{name:"jichang"}],
+// 	shenzhen:[{name:"huaqiangbei"},{name:"dameisha"},{name:"xiaomeisha"}]};
 
-// var data = {
-// 	cities:[{name:"beijing",places:[{name:"changcheng"},{name:"gugong"},{name:"sanlitun"},{name:"yiheyuan"}]},
-// 	{name:"shanghai",places:[{name:"yuyuan"},{name:"waitan"},{name:"dongwuyuan"},{name:"jichang"}]},
-// 	{name:"shenzhen",places:[{name:"huaqiangbei"},{name:"dameisha"},{name:"xiaomeisha"}]}]
-// }
+// app.get('/cities',function(req,res){
+// 	res.send({cities:Object.keys(data)});
+// });
 
+// app.get('/cities/:city_name',function(req,res){
+// 	var city = req.params.city_name;
+// 	res.send({places:data[city]});
+// });
 
-app.get('/cities',function(req,res){
-	res.send({cities:Object.keys(data)});
+var geology = landscape.Landscape;
+
+app.get('/provinces',function(req,res){
+	res.send({provinces:Object.keys(geology)});
 });
 
-app.get('/cities/:city_name',function(req,res){
-	var city = req.params.city_name;
-	res.send({places:data[city]});
+app.get('/cities/:province_name',function(req,res){
+	var province_name = req.params.province_name;
+	res.send({cities:Object.keys(geology[province_name])});
+});
+
+app.get('/counties/:province_name/:city_name',function(req,res){
+	var province_name = req.params.province_name;
+	var city_name = req.params.city_name;
+	res.send({cities:geology[province_name][city_name]});
 });
 
 app.get('/',function(req,res){
